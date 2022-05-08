d708aac0bf9e793feff85f8776f7f4b65acaae28
great-park <cksgh1735@naver.com>
2022-05-09 01:07:10 +0900

    왜 스프링이 필요한가? - MemberServiceImpl가 DIP를 위배하고 있음

--- 2. park-spring/src/main/java/park/core/member/MemberServiceImpl.java
+++ 2. park-spring/src/main/java/park/core/member/MemberServiceImpl.java
@@ -2,6 +2,7 @@ package park.core.member;
 
 public class MemberServiceImpl implements MemberService{
 
+    // MemberServiceImpl는 (추상화)MemberRepository도 의존하고, 구현체인 MemmoryMemberRepository도 의존하기 때문에 원칙에  DIP 위배!
     private final MemberRepository memberRepository = new MemoryMemberRepository();
     @Override
     public void join(Member member) {
