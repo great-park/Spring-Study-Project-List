d9511f17b3f3db20a66d014769ddab5d1950332a
great-park <cksgh1735@naver.com>
2022-05-09 15:22:25 +0900

    OrderServiceImpl이 구현 클래스에도 의존->DIP 위배. 인터페이스만 의존하도록 변경하고 누군가가 구현 객체를 대신 생성하고 주입해야한다. DI

---
+++ 2. park-spring/src/main/java/park/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,14 @@
+package park.core.discount;
+import park.core.member.Grade;
+import park.core.member.Member;
+public class RateDiscountPolicy implements DiscountPolicy {
+    private int discountPercent = 10; //10% 할인
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
