803eb8ed3c1a6c6eb781752e27498035f8484470
great-park <cksgh1735@naver.com>
2022-05-10 00:30:57 +0900

    AppConfig 변경

--- 2. park-spring/src/main/java/park/core/AppConfig.java
+++ 2. park-spring/src/main/java/park/core/AppConfig.java
@@ -1,5 +1,7 @@
 package park.core;
+import park.core.discount.DiscountPolicy;
 import park.core.discount.FixDiscountPolicy;
+import park.core.member.MemberRepository;
 import park.core.member.MemberService;
 import park.core.member.MemberServiceImpl;
 import park.core.member.MemoryMemberRepository;
@@ -12,11 +14,15 @@ public class AppConfig {
     //누군가 MemberService를 부르면, MemoryMemberRepository 객체를 생성하고
     //생성한 객체 인스턴스의 참조값을 넘겨 주어 생성자를 통해 주입해준다.
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
     public OrderService orderService() {
-        return new OrderServiceImpl(
-                new MemoryMemberRepository(),
-                new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
