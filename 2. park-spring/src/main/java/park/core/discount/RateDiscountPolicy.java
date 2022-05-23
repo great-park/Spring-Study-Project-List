a569045a13ce5a88be741b003f235eb1b82361b9
great-park <cksgh1735@naver.com>
2022-05-24 02:48:09 +0900

    annotation 만들기

--- 2. park-spring/src/main/java/park/core/discount/RateDiscountPolicy.java
+++ 2. park-spring/src/main/java/park/core/discount/RateDiscountPolicy.java
@@ -1,9 +1,11 @@
 package park.core.discount;
 import org.springframework.stereotype.Component;
+import park.core.annotation.MainDiscountPolicy;
 import park.core.member.Grade;
 import park.core.member.Member;
 
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
     private int discountPercent = 10; //10% 할인
     @Override
--- 2. park-spring/src/main/java/park/core/order/OrderServiceImpl.java
+++ 2. park-spring/src/main/java/park/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package park.core.order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import park.core.annotation.MainDiscountPolicy;
 import park.core.discount.DiscountPolicy;
 import park.core.discount.FixDiscountPolicy;
 import park.core.discount.RateDiscountPolicy;
@@ -10,13 +11,17 @@ import park.core.member.MemberRepository;
 import park.core.member.MemoryMemberRepository;
 
 @Component
-@RequiredArgsConstructor
-//lombok을 이용해서 final이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다!
-//생성자가 "하나"이기 때문에 @Autowired를 생략하더라도 자동으로 의존관계 주입이 된다.
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
+
+    //생성자 자동 주입
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
    @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
