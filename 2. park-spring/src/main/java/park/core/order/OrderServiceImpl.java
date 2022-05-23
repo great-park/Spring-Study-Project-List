754fe594d658c3bca3442824fefe5a3cf7f3a69e
great-park <cksgh1735@naver.com>
2022-05-24 02:09:29 +0900

    lombok 사용

--- 2. park-spring/src/main/java/park/core/order/OrderServiceImpl.java
+++ 2. park-spring/src/main/java/park/core/order/OrderServiceImpl.java
@@ -1,4 +1,5 @@
 package park.core.order;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import park.core.discount.DiscountPolicy;
@@ -9,18 +10,13 @@ import park.core.member.MemberRepository;
 import park.core.member.MemoryMemberRepository;
 
 @Component
+@RequiredArgsConstructor
+//lombok을 이용해서 final이 붙은 필드를 모아서 생성자를 자동으로 만들어 준다!
+//생성자가 "하나"이기 때문에 @Autowired를 생략하더라도 자동으로 의존관계 주입이 된다.
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    //private final DiscountPolicy discountPolicy; 로 변경하면  null pointer exception 발생생
 
    @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
